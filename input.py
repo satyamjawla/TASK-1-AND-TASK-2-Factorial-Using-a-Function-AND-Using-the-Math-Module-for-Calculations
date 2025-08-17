7a29154e433a5eec8e1c5cf4e0710ed58f4a4094
satyamjawla <satyamjawla123@gmail.com>
2025-08-17 15:20:56 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,9 @@
+import math
+
+number = float(input("Enter a number: "))
+
+print("Square root:",math.sqrt(number))
+
+print("Natural logarithm:",math.log(number))
+
+print("Sine (radians):",math.sin(number))
